31ff7471967c891899578bc92b6083bd412f41ef
vimaik <victormg2k@gmail.com>
2023-09-07 11:20:59 +0200

    07. Teleports (Ejemplo sin teleport)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <button @click="show = !show">Menú</button>
-  <transition name="fade">
-    <Menu v-show="show" />
-  </transition>
+  <div>
+    <Modal />
+  </div>
 </template>
 
 <script>
-import Menu from "./components/Menu.vue";
+import Modal from "./components/Modal.vue";
 export default {
   name: "App",
   components: {
-    Menu,
-  },
-  data() {
-    return {
-      show: false,
-    };
+    Modal,
   },
 };
 </script>
@@ -29,30 +23,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-leave-active,
-.fade-enter-active {
-  transition: opacity 0.5s ease;
-}
-/* 
-OPCIONES DEL EJEMPLO DE EFECTO DE TRANSISICÓN 'fade'
-
-fade-enter-from
-fade-enter-active
-fade-enter-to
-fade-leave-from
-fade-leave-active
-fade-leave-to
-
-dónde,
-  'fade' correspondeal efecto de transición a aplicar
-  'enter' corresponde al elemento entrante cuando dse activa ese efecto de transición
-  'leave' corresponde al elemento saliente cuando dse activa ese efecto de transición
-  'from' corresponde al momento en el que se inicia el efecto de transición
-  'active' corresponde al momento durante el cual la transición está activa
-  'to' corresponde al momento en el que la transición ha acabado
-*/
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <div v-show="show" class="modal">
+      <h1>Título</h1>
+      <p>Texto informativo del modal</p>
+      <button @click="toggle">Cerrar</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+<style></style>
