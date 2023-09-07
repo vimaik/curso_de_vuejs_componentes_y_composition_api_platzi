414e42a76c3493b827ebe2903c15037717bdc186
vimaik <victormg2k@gmail.com>
2023-09-07 10:57:40 +0200

    06. Transiciones

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="show = !show">Menú</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Menu from "./components/Menu.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Menu,
+  },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>
@@ -23,4 +29,30 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
+}
+/* 
+OPCIONES DEL EJEMPLO DE EFECTO DE TRANSISICÓN 'fade'
+
+fade-enter-from
+fade-enter-active
+fade-enter-to
+fade-leave-from
+fade-leave-active
+fade-leave-to
+
+dónde,
+  'fade' correspondeal efecto de transición a aplicar
+  'enter' corresponde al elemento entrante cuando dse activa ese efecto de transición
+  'leave' corresponde al elemento saliente cuando dse activa ese efecto de transición
+  'from' corresponde al momento en el que se inicia el efecto de transición
+  'active' corresponde al momento durante el cual la transición está activa
+  'to' corresponde al momento en el que la transición ha acabado
+*/
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li>Opción 1</li>
+    <li>Opción 2</li>
+    <li>Opción 3</li>
+  </ul>
+</template>
+
+<script></script>
+
+<style></style>
